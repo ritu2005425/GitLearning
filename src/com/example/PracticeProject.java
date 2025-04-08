f17eb60211c03a89625b7fc6f277b2363bb96642
ritu2005425 <ritujadhav67@gmail.com>
2025-04-08 23:04:33 +0530

    add

--- src/com/example/PracticeProject.java
+++ src/com/example/PracticeProject.java
@@ -11,7 +11,13 @@ public class PracticeProject {
 				System.out.println((i+1)+"Here is the i's value");
 			}
 		}
-
+		for(int i=0;i<100;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println((i+1)+"Here is the i's value");
+			}
+		}
 		System.out.println("this is to learn pull command-->after you emter pull command this chnges will come inside local");
 	}
 
