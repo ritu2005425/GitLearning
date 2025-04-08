213cb9ce2d4c968a13f8a50e9205ec2412fed2e2
ritu2005425 <ritujadhav67@gmail.com>
2025-04-08 09:13:44 +0530

    first commit

---
+++ src/com/example/PracticeProject.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class PracticeProject {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<100;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
