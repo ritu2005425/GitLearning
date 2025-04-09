832d6c14ba3b4487310041dfbb2f6066c4881495
ritu2005425 <ritujadhav67@gmail.com>
2025-04-09 09:14:42 +0530

    sec

--- src/com/example/PracticeProject.java
+++ src/com/example/PracticeProject.java
@@ -4,8 +4,8 @@ public class PracticeProject {
 
 	public static void main(String[] args) {
 
-		int a=10;
-		System.out.println("hello this is from master");
+		int a=90;
+		System.out.println("hello this is from master newesty branch");
 	}
 
 }
